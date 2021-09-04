dde17bbade08bde16ecbe2ea7d98ed57c6f4416a
Jonathan-Elias <64284629+Jonathan-Elias@users.noreply.github.com>
2021-09-04 19:59:27 -0300

    Update scan_diretorios.sh

--- scan_diretorios.sh
+++ scan_diretorios.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for scan in $(cat common.txt)
 do
-final=$(curl -s -H "User-Agent: SEVEN" -o /dev/null -w "%{http_code}" http://$1/$scan.$2)
+#final=$(curl -s -H "User-Agent: SEVEN" -o /dev/null -w "%{http_code}" http://$1/$scan.$2)
+final=$(curl -s -H "User-Agent: SEVEN" -o /dev/null -w "%{http_code}" http://$1/$scan)
 if [ $final == "200" ]
 then
 echo "Diretorio encontrado: $1/$scan/"
