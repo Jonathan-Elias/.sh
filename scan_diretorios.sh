76301ba5dd2a47f7c2f228d117e67f282bc26b51
Jonathan-Elias <64284629+Jonathan-Elias@users.noreply.github.com>
2021-09-04 19:55:37 -0300

    Add files via upload

---
+++ scan_diretorios.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for scan in $(cat common.txt)
+do
+final=$(curl -s -H "User-Agent: SEVEN" -o /dev/null -w "%{http_code}" http://$1/$scan.$2)
+if [ $final == "200" ]
+then
+echo "Diretorio encontrado: $1/$scan/"
+fi
+done
